11997cd956bb47bb6371868cc0e3d0feca9129ae
sandy99405 <sandysandiya99@gmail.com>
2022-09-27 14:10:02 +0530

    Commiting all files successfully

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+class Solution{
+      String firstName;
+      
+      String lastName;
+      
+      
+}
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             
+		HashMap<Integer,Integer> hm = new LinkedHashMap<>();
+      
+        hm.put(1,3);
+        hm.put(2,4);
+        hm.put(4,5);
+        
+        
+	}
+
+}
